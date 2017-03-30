e6959fc1ca05de9f046a1e3d53cecb65c0489970
itay <itay83ko@gmail.com>
2017-03-30 14:39:25 +0300

    changed to apartments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { FlightSearchComponent } from './flight-search.comopnent/flight-search.component';
-import { HotelsSearchComponent } from './hotels-search.component/hotels-search.component'
+import { ApartmentSearch } from './apartment-search.component/apartment-search.component';
+import { SearchResultComponent } from './search-result.component/search-result.component';
 
 const routes: Routes = [
-    { path: '', redirectTo: '/flights', pathMatch: 'full' },
-    { path: 'flights', component: FlightSearchComponent },
-    {path:'hotels', component:HotelsSearchComponent}
+    { path: '', redirectTo: '/search', pathMatch: 'full' },
+    { path: 'search', component: ApartmentSearch },
+    { path: 'results', component: SearchResultComponent},
 ]
 
 
